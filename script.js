2e536c8296d457e200b9b36938d10dce0547a920
wiphy <lm802085@gmail.com>
2024-02-16 11:35:18 +0100

    add exercices filter

---
+++ script.js
@@ -0,0 +1,19 @@
+// Exercice 1
+// filtrer le tableau numbers pour ne garder que les éléments >= 3
+const numbers = [1, 2, 3, 4, 5];
+
+// Exercice 2
+// filtrer le tableau profils pour ne garder que les éléments avec un age > 30
+const profils = [
+  { name: "Alice", age: 25, categoryId: 1 },
+  { name: "Bob", age: 30, categoryId: 1 },
+  { name: "Charlie", age: 35, categoryId: 2 },
+  { name: "David", age: 40, categoryID: 2 },
+];
+
+// Exercice 3
+// filtrer le tableau profils pour ne garder que les éléments avec le categoryId correspondant à celui de développeur dans le tableau categories
+const categories = [
+  { id: 1, name: "Développeur" },
+  { id: 2, name: "Manager" },
+];
